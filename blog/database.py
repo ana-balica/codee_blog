608f6b938eaf52141b67a08a502e893ee4a056fe
ana-balica <ana.balica@gmail.com>
2013-04-17 10:15:30 +0300

    reindent

--- blog/database.py
+++ blog/database.py
@@ -10,8 +10,8 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_db()
-    import blog.models
-    Base.metadata.create_all(bind=engine)
+  # import all modules here that might define models so that
+  # they will be registered properly on the metadata.  Otherwise
+  # you will have to import them first before calling init_db()
+  import blog.models
+  Base.metadata.create_all(bind=engine)
