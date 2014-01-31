892242afe836df61b04a697509236865565942c1
ana-balica <ana.balica@gmail.com>
2014-01-31 03:20:16 +0100

    use 4 spaceses for indent in js code

--- blog/static/js/script.js
+++ blog/static/js/script.js
@@ -1,29 +1,29 @@
 $(document).ready(function () {
 
-  $("body").on('click', '.sidebar ul a, a#index, a.read_more, a.pagination_', function (e) {
-    e.preventDefault();
-    var page_url = $(e.currentTarget).attr('href');
-    $.ajax({
-      type: 'GET',
-      url: $SCRIPT_ROOT + page_url,
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function (data) {
-        $('#content').hide().html(data['data']).fadeIn('slow');
-        // update the page title
-        $('title').html(data['title']);
-        // update the URL
-        window.history.pushState("", "", page_url);
-        // scroll to top
-        window.scrollTo(0, 0);
-        // syntax highlight everything on page
-        Rainbow.color();
-      }
+    $("body").on('click', '.sidebar ul a, a#index, a.read_more, a.pagination_', function (e) {
+        e.preventDefault();
+        var page_url = $(e.currentTarget).attr('href');
+        $.ajax({
+            type: 'GET',
+            url: $SCRIPT_ROOT + page_url,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                $('#content').hide().html(data['data']).fadeIn('slow');
+                // update the page title
+                $('title').html(data['title']);
+                // update the URL
+                window.history.pushState("", "", page_url);
+                // scroll to top
+                window.scrollTo(0, 0);
+                // syntax highlight everything on page
+                Rainbow.color();
+            }
+        });
     });
-  });
 });
 
 // shorter version of console.log
 function c(val) {
-  console.log(val);
+    console.log(val);
 }
